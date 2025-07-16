384dd02c27b1bcfff175a52ad137e1454ec62a1d
Nahuel <nahueltecnicomdp@gmail.com>
2025-07-16 07:06:53 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/serverless"; // 👈 O vercel/edge si preferís
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  output: "server", // Requerido para usar SSR
+  adapter: vercel({}),
 });
