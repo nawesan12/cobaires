0ced2a1eedf8747f04cec949db5ab3b88838e948
Nahuel <nahueltecnicomdp@gmail.com>
2025-07-21 14:26:19 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
-import { addDynamicIconSelectors } from "@iconify/tailwind";
+import vercel from "@astrojs/vercel"; // 👈 O vercel/edge si preferís
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      tailwindcss(),
-      //@ts-expect-error bla
-      addDynamicIconSelectors(),
-      //@ts-expect-error bla
-      function ({ addVariant }) {
-        addVariant("active", "&.is-active");
-      },
-    ],
+    plugins: [tailwindcss()],
   },
   output: "server", // Requerido para usar SSR
   adapter: vercel(),
